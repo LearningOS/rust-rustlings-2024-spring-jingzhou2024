eeddd67bb0fee3e4749b3b78f7551681f6d76793
jingzhou2024 <yuqingyuzhuo@proton.me>
2024-04-12 18:36:28 +0800

    答题

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,12 +13,20 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
 
 // Don't modify this function!
+
+fn calculate_price_of_apples(value: u32) -> u32{
+    if value <= 40 {
+        value * 2
+    } else {
+        value
+    }
+}
+
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
